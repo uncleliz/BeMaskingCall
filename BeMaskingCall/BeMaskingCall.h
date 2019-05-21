4b1fca2ae2308110c07102290d2bc56c3ec12f95
manh.le <manh.le@be.xyz>
2019-05-21 23:38:12 +0700

    Fist Commint

---
+++ BeMaskingCall/BeMaskingCall.h
@@ -0,0 +1,19 @@
+//
+//  BeMaskingCall.h
+//  BeMaskingCall
+//
+//  Created by manh.le on 5/21/19.
+//  Copyright © 2019 manh.le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BeMaskingCall.
+FOUNDATION_EXPORT double BeMaskingCallVersionNumber;
+
+//! Project version string for BeMaskingCall.
+FOUNDATION_EXPORT const unsigned char BeMaskingCallVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BeMaskingCall/PublicHeader.h>
+
+
